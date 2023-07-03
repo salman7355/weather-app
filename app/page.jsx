f3e762558c5b92d3165c99aa91cd7decfb770619
salman7355 <salmanmohamed837@gmail.com>
2023-07-03 08:23:30 +0300

    initial commit

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+"use client";
+import ActionBar from "@/Components/ActionBar";
+import Degree from "@/Components/Degree";
+
+export default function Home() {
+  return (
+    <div>
+      <ActionBar />
+      <Degree />
+    </div>
+  );
+}
