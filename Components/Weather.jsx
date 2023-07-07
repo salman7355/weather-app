583317e408c8beefc9c15570c8965581a295d1cf
salman7355 <salmanmohamed837@gmail.com>
2023-07-07 17:09:02 +0300

    Done

---
+++ Components/Weather.jsx
@@ -0,0 +1,45 @@
+import Image from "next/image";
+import React from "react";
+
+const Weather = ({ weather }) => {
+  console.log(weather);
+
+  return (
+    <div className="relative flex flex-col justify-between max-w-[500px] w-full h-[86vh] m-auto p-4 text-gray-300 z-10">
+      <div className="relative flex  justify-between pt-12">
+        <div>
+          <Image
+            src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+            alt="/"
+            width="100"
+            height="100"
+          />
+          <p className="text-2xl">{weather.weather[0].description}</p>
+        </div>
+        <div className="text-9xl">{weather.main.temp.toFixed(0)}&#176;</div>
+      </div>
+
+      <div className="bg-black/50 relative text-gray-200 mb-10 p-8  rounded-xl">
+        <p className="text-center font-bold pb-6 text-2xl">
+          Weather in {weather.name}
+        </p>
+        <div className="flex justify-between text-center text-xl font-bold">
+          <div>
+            <p>{weather.main.feels_like.toFixed(0)}&#176;</p>
+            <p>Feel Likes</p>
+          </div>
+          <div>
+            <p>{weather.main.humidity}%</p>
+            <p>Humidity</p>
+          </div>
+          <div>
+            <p>{weather.wind.speed.toFixed(0)} MPS</p>
+            <p>Winds</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Weather;
--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,61 @@
 "use client";
-import ActionBar from "@/Components/ActionBar";
-import Degree from "@/Components/Degree";
+import Spinner from "@/Components/Spinner";
+import Weather from "@/Components/Weather";
+import { WeatherAPI } from "@/apiKey";
+import axios from "axios";
+import Image from "next/image";
+import { useState } from "react";
+import { AiOutlineSearch } from "react-icons/ai";
 
 export default function Home() {
-  return (
-    <div>
-      <ActionBar />
-      <Degree />
-    </div>
-  );
+  const [City, setCity] = useState("");
+  const [weather, setWeather] = useState("");
+  const [Loading, setLoading] = useState(false);
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${City}&appid=${WeatherAPI}&units=metric`;
+
+  const fetchData = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    axios.get(url).then((response) => {
+      setWeather(response.data);
+    });
+    setCity("");
+    setLoading(false);
+  };
+
+  if (Loading) {
+    return <Spinner />;
+  } else {
+    return (
+      <div>
+        <div className="absolute inset-0 bg-black/20  z-[1]" />
+        <Image
+          src="https://images.unsplash.com/photo-1580193769210-b8d1c049a7d9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1174&q=80"
+          alt="/"
+          fill={true}
+          className="object-cover"
+        />
+
+        <div className="flex justify-center items-center max-w-[500px] w-full m-auto pt-9 px-4 text-white relative z-10">
+          <form
+            onSubmit={fetchData}
+            className="mr-3 px-4 border-solid rounded-2xl border-white border-2 flex justify-between p-3 items-center"
+          >
+            <input
+              onChange={(e) => setCity(e.target.value)}
+              type="text"
+              placeholder="Search City"
+              className=" bg-transparent border-none text-white focus:outline-none text-xl"
+            />
+            <button onClick={fetchData}>
+              <AiOutlineSearch color="white" size={30} />
+            </button>
+          </form>
+        </div>
+
+        {weather.main && <Weather weather={weather} />}
+      </div>
+    );
+  }
 }
