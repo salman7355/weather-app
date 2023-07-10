575f340ec661053c3a8d0bb1f6a618b9da84f87b
salman7355 <salmanmohamed837@gmail.com>
2023-07-10 15:50:41 +0300

    Deployment errors

--- Components/Weather.jsx
+++ Components/Weather.jsx
@@ -5,7 +5,7 @@ const Weather = ({ weather }) => {
   console.log(weather);
 
   return (
-    <div className="relative flex flex-col justify-between max-w-[500px] w-full h-[90vh] m-auto p-4 text-gray-300 z-10">
+    <div className="relative flex flex-col justify-between max-w-[500px] w-full h-[85vh] m-auto p-4 text-gray-300 z-10">
       <div className="relative flex  justify-between pt-12">
         <div>
           <Image
@@ -19,7 +19,7 @@ const Weather = ({ weather }) => {
         <div className="text-9xl">{weather.main.temp.toFixed(0)}&#176;</div>
       </div>
 
-      <div className="bg-black/50 relative text-gray-200 mb-10 p-8  rounded-xl">
+      <div className="bg-black/50 text-gray-200 mb-10 p-8 rounded-xl">
         <p className="text-center font-bold pb-6 text-2xl">
           Weather in {weather.name}
         </p>
