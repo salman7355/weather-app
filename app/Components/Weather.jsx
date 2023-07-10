38927f650e689cc87743241b9f8074f5016053d7
salman7355 <salmanmohamed837@gmail.com>
2023-07-10 16:09:57 +0300

    Done

--- app/Components/Weather.jsx
+++ app/Components/Weather.jsx
@@ -1,13 +1,10 @@
-"use client";
 import Image from "next/image";
 import React from "react";
 
 const Weather = ({ weather }) => {
-  console.log(weather);
-
   return (
     <div className="relative flex flex-col justify-between max-w-[500px] w-full h-[85vh] m-auto p-4 text-gray-300 z-10">
-      <div className="relative flex  justify-between pt-12">
+      <div className="relative flex justify-between pt-12">
         <div>
           <Image
             src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
